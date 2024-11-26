a4347ac1fd930b716551c1b239b348358d9d073e
edilson591 <edilson591@gmail.com>
2024-11-26 10:28:07 -0300

    add informação no json

--- src/server.ts
+++ src/server.ts
@@ -3,17 +3,25 @@ import { routes } from "./routes";
 import fastifyCors from "@fastify/cors";
 
 
-const app = fastify({ logger: true });
+const app = fastify({ 
+  logger: true,
+  ignoreTrailingSlash: true,
+  caseSensitive: false,
+  connectionTimeout: 30000,
+});
 
 const start = async () => {
   app.setErrorHandler((error, request, reply) => {
     reply.code(400).send({ message: error.message });
   });
-  await app.register(fastifyCors);
+  await app.register(fastifyCors,{
+    origin: true, 
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+  });
   await app.register(routes);
 
   try {
-    await app.listen({ port: 3020 });
+    await app.listen({ port: process.env.PORT ? Number(process.env.PORT) : 3020 });
   } catch (error) {
     process.exit(1);
   }
