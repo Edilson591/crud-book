5d5cf40b68f16e362a18ce3b151023fa47708067
edilson591 <edilson591@gmail.com>
2024-11-26 10:48:55 -0300

    upgrade port

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,7 @@ const start = async () => {
   await app.register(routes);
 
   try {
-    await app.listen({ port: process.env.PORT ? Number(process.env.PORT) : 3020 });
+    await app.listen({ port: 3020 });
   } catch (error) {
     process.exit(1);
   }
